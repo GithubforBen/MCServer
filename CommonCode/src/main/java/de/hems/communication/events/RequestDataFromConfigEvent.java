fce913ac4353b2007ea64f35af47ddfb7919e638
Ben S. <95373251+GithubforBen@users.noreply.github.com>
2025-10-18 14:25:33 +0200

    ```markdown feat: Simplify constructor of `RequestDataFromConfigEvent` by removing unnecessary parameters ```

--- CommonCode/src/main/java/de/hems/communication/events/RequestDataFromConfigEvent.java
+++ CommonCode/src/main/java/de/hems/communication/events/RequestDataFromConfigEvent.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 public class RequestDataFromConfigEvent extends EventFoundationData implements Event {
     private final String key;
 
-    public RequestDataFromConfigEvent(String sender, String receiver, String key) {
-        super(sender, receiver, UUID.randomUUID());
+    public RequestDataFromConfigEvent(String sender, String key) {
+        super(sender, "ServerLauncher", UUID.randomUUID());
         this.key = key;
     }
 
