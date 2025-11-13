41c0bb2c1f726e708e83cd7a15cad7a7f680841a
Ben <01bensch@gmail.com>
2025-11-13 12:28:43 +0100

    Refactor server and inventory management systems, enhance event handling
    - Replace `RespondPlayerMoneyEvent` inheritance to extend `RespondDataEvent` for consistency.
    - Add new custom inventory types (`SERVERCREATEINVENTORY`, `SERVERSTARTINVENTORY`) for interactive server creation and management.
    - Update `RequestPlayerMoneyEventHandler` to use `eventId` instead of `playerId`.
    - Introduce UI components for managing server resources and actions (`+`, `-`, Start).
    - Refactor imports in `InventoryBase` for cleaner organization.

--- CommonCode/src/main/java/de/hems/communication/events/money/RespondPlayerMoneyEvent.java
+++ CommonCode/src/main/java/de/hems/communication/events/money/RespondPlayerMoneyEvent.java
@@ -2,30 +2,18 @@ package de.hems.communication.events.money;
 
 import de.hems.communication.ListenerAdapter;
 import de.hems.communication.events.types.Event;
-import de.hems.communication.events.types.EventFoundationData;
+import de.hems.communication.events.types.RespondDataEvent;
 
 import java.io.Serializable;
 import java.util.UUID;
 
-public class RespondPlayerMoneyEvent extends EventFoundationData implements Event, Serializable {
+public class RespondPlayerMoneyEvent extends RespondDataEvent implements Event, Serializable {
     private static final long serialVersionUID = 4L;
-    private  UUID playerId;
-    private  int money;
 
-    public RespondPlayerMoneyEvent(ListenerAdapter.ServerName receiver, UUID playerId, int money) {
-        super(receiver);
-        this.playerId = playerId;
-        this.money = money;
+    public RespondPlayerMoneyEvent(ListenerAdapter.ServerName receiver, UUID requestID, int money) {
+        super(receiver, Integer.valueOf(money), requestID);
     }
 
     public RespondPlayerMoneyEvent() {
-    }
-
-    public UUID getPlayerId() {
-        return playerId;
-    }
-
-    public int getMoney() {
-        return money;
     }
 }
