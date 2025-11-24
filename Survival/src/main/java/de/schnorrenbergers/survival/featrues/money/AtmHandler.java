bc86f7c0c69388616f1dbcc580408435fc5dc0e0
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-11-24 12:15:24 +0100

    happy ben, happy life

---
+++ Survival/src/main/java/de/schnorrenbergers/survival/featrues/money/AtmHandler.java
@@ -0,0 +1,47 @@
+package de.schnorrenbergers.survival.featrues.money;
+
+import de.hems.api.ItemApi;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class AtmHandler {
+    public static void deposit(Player player, int amount) {
+        boolean invContains = player.getInventory().containsAtLeast(ItemStack.of(MoneyHandler.MONEY_ITEM), amount);
+        if(invContains) {
+            removeItems(player.getInventory(), MoneyHandler.MONEY_ITEM, amount);
+            MoneyHandler.addMoney(amount, player.getUniqueId());
+            return;
+        }
+    }
+
+    public static boolean payout(Player player, int amount) {
+        boolean removedMoney = MoneyHandler.removeMoney(amount, player.getUniqueId());
+        if(removedMoney) {
+            ItemStack itemStack = new ItemApi(MoneyHandler.MONEY_ITEM, amount).build();
+            player.getInventory().addItem(itemStack);
+        }
+        return removedMoney;
+    }
+
+    private static void removeItems(Inventory inventory, Material type, int amount) {
+        if (amount <= 0) return;
+        int size = inventory.getSize();
+        for (int slot = 0; slot < size; slot++) {
+            ItemStack is = inventory.getItem(slot);
+            if (is == null) continue;
+            if (type == is.getType()) {
+                int newAmount = is.getAmount() - amount;
+                if (newAmount > 0) {
+                    is.setAmount(newAmount);
+                    break;
+                } else {
+                    inventory.clear(slot);
+                    amount = -newAmount;
+                    if (amount == 0) break;
+                }
+            }
+        }
+    }
+}
