ee598f510a0bc2f18f849dcca26b83a6aad542cf
ben <01bensch@gmail.com>
2025-12-18 20:16:41 +0100

    Restrict `/banane` command usage to operators only.

--- Survival/src/main/java/de/schnorrenbergers/survival/listener/JoinListener.java
+++ Survival/src/main/java/de/schnorrenbergers/survival/listener/JoinListener.java
@@ -1,7 +1,40 @@
 package de.schnorrenbergers.survival.listener;
 
+import de.schnorrenbergers.survival.Survival;
 import jdk.jfr.Label;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.persistence.PersistentDataType;
 
 public class JoinListener implements Listener {
+
+    private static boolean registered = false;
+
+    public JoinListener() {
+        if (registered) {
+            return;
+        }
+        Bukkit.getPluginManager().registerEvents(this, Survival.getInstance());
+        registered = true;
+    }
+
+    @EventHandler
+    public void onJoin(org.bukkit.event.player.PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        e.setJoinMessage(ChatColor.GREEN +">>" + player.getName());
+        if (!player.getPersistentDataContainer().has(NamespacedKey.fromString("spawn"))) {
+            player.teleport(player.getWorld().getSpawnLocation());
+            player.getPersistentDataContainer().set(NamespacedKey.fromString("spawn"), PersistentDataType.STRING, "true");
+            System.out.println("Spawn teleported for " + player.getName());
+        }
+    }
+
+    @EventHandler
+    public void onQuit(org.bukkit.event.player.PlayerQuitEvent e) {
+        e.setQuitMessage(ChatColor.RED + "<<"+ e.getPlayer().getName());
+    }
 }
